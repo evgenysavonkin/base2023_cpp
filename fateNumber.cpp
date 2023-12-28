8744ca323efc66d6b54a88220b2a1095b6620292
Evgeny Savonkin <evgenysavonkin@gmail.com>
2023-12-28 18:12:08 +0300

    Init commit. Done 5 tasks from 1st module

---
+++ fateNumber.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+const int maxDayValue = 31;
+const int minDayValue = 0;
+const int minMonthValue = 0;
+const int maxMonthValue = 12;
+
+int main() {
+    int day = 0;
+    int month = 0;
+    int year = 0;
+    cout << "Enter day: " << endl;
+    cin >> day;
+    if (!(day > minDayValue && day <= maxDayValue)) {
+        cout << "Incorrect day. Must be in a range of (01-31)";
+        return -1;
+    }
+    cout << "Enter month: " << endl;
+    cin >> month;
+    if (!(month > minMonthValue && month <= maxMonthValue)) {
+        cout << "Incorrect month. Must be in a range of (01-12)";
+        return -1;
+    }
+    cout << "Enter year: " << endl;
+    cin >> year;
+    int sum = 0;
+    if (day < 10) {
+        sum += day;
+    } else {
+        sum += day / 10;
+        sum += day % 10;
+    }
+    if (month < 10) {
+        sum += month;
+    } else {
+        sum += month / 10;
+        sum += month % 10;
+    }
+    sum += year / 1000;
+    sum += year / 100 % 10;
+    sum += year / 10 % 10;
+    sum += year % 10;
+    if (sum < 10) {
+        cout << "Fate number is " << sum;
+        return 0;
+    }
+    int result = sum / 10;
+    result += sum % 10;
+    cout << "Fate number is " << result;
+    return 0;
+}
+
---
+++ multiple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+const int iRange = 10;
+const int jRange = 10;
+
+using namespace std;
+
+int main() {
+    int j = 1;
+    for (int i = 1; i <= iRange; i++) {
+        while (j <= jRange) {
+            cout.width(4);
+            cout << i * j << "  ";
+            j++;
+        }
+        j = 1;
+        cout << endl;
+    }
+    return 0;
+}
